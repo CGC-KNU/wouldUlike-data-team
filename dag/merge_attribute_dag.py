76ba976d788ad492980114676e7e5b6c5243e780
Elisha0510 <sjeong35@knu.ac.kr>
2024-12-05 17:23:08 +0900

    feat: redshift 테이블 생성하는 쿼리, Task 생성

--- dag/merge_attribute_dag.py
+++ dag/merge_attribute_dag.py
@@ -22,3 +22,31 @@ dag = DAG(
     schedule=None,
     catchup=False
 )
+
+CREATE_QUERY = '''
+CREATE TABLE IF NOT EXISTS public.food_attribute (
+    name VARCHAR(255) NOT NULL, 
+    status VARCHAR(50),
+    address_zip_code VARCHAR(50),
+    road_zip_code VARCHAR(25),
+    road_full_address VARCHAR(500),
+    road_address VARCHAR(500),
+    x DOUBLE PRECISION,
+    y DOUBLE PRECISION,
+    phone_number VARCHAR(50),
+    category_1 VARCHAR(50),
+    category_2 VARCHAR(50),
+    district_name VARCHAR(50),
+    attribute_1 VARCHAR(5),
+    attribute_2 VARCHAR(5),
+    attribute_3 VARCHAR(5),
+    attribute_4 VARCHAR(5)
+);
+'''
+
+create_food_attribute_table_task = PostgresOperator(
+    task_id = "create_food_attribute_table",
+    postgres_conn_id ='wouldUlike-redshift',
+    sql = CREATE_QUERY,
+    dag = dag
+)
